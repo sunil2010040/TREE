2d3e7ba138ff40e63704e88fb54281aa2a665bab
Sunil Kumar Chakravartty <91359079+sunil2010040@users.noreply.github.com>
2023-02-04 12:01:56 +0530

    Add files via upload

---
+++ left view of binary tree.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node*left;
+    struct node*right;
+    
+    node(int value)
+    {
+        data =value;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+void left_view(node*root)
+{
+    if(root==NULL)
+    {
+        return ;
+    }
+    
+    queue<node*>q;
+    q.push(root);
+    
+    while(!q.empty())
+    {
+        int n=q.size();
+        for(int i=0;i<n;i++)
+        {
+            node*current=q.front();
+            q.pop();
+            if(i==0)
+            {
+                cout<<current->data<<" ";
+            }
+            
+            if(current->left!=NULL)
+            {
+                q.push(current->left);
+            }
+            
+            if(current->right!=NULL)
+            {
+                q.push(current->right);
+            }
+        }
+    }
+}
+
+int main()
+{
+    node*root=new node(1);
+    root->left=new node(2);
+    root->right=new node(3);
+    
+    root->right->left=new node(4);
+    root->right->right=new node(5);
+    root->right->left->right=new node(6);
+  
+    left_view(root);
+}
